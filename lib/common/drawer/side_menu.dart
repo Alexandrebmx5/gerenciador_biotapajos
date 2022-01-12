39b43edf3e166ba81322e6dc843753ec1cc824d1
alexandrebmx5 <alexandrebmx5@gmail.com>
2022-01-12 19:48:26 -0300

    build version 1.0.2+8

--- lib/common/drawer/side_menu.dart
+++ lib/common/drawer/side_menu.dart
@@ -134,7 +134,7 @@ class SideMenu extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Text(
-                      'v. 1.0.2+7',
+                      'v. 1.0.2+8',
                       style: TextStyle(
                           color: Colors.black,
                           fontFamily: 'Principal',
